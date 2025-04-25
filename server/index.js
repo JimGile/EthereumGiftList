51eb777956cd18e115a6aba4ada19e4cfc28b3ca
Jim Gile <jim@jimgile.com>
2025-04-25 15:09:34 -0600

    working solution

--- server/index.js
+++ server/index.js
@@ -6,16 +6,17 @@ const port = 1225;
 const app = express();
 app.use(express.json());
 
-// TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// hardcode a merkle root here representing the whole nice list
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { proof, name } = req.body;
+  console.log(`Verifying name: ${name}.`);
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  // Prove that a name is in the list 
+  // verify proof against the Merkle Root
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
